d35f071cc323cec033de15d995c83df1f87f06f2
MJMODS <95528082+MJMODS@users.noreply.github.com>
2021-12-06 23:07:29 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'PIKACHU;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'MJ-SIR;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
